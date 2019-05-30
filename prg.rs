d6c89e3871ea19730a31e9e90fe87a6d491ef5c4
Rajdeep Bandopadhyay <rajdeepbandopadhyay@gmail.com>
2019-05-30 09:47:20 +0530

    input simplification

--- prg.rs
+++ prg.rs
@@ -1,13 +1,22 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
+use std::fs;
+use simple_user_input::get_input;
 
-fn read_a_file(path: String){
-let data = fs::read_to_string(path).expect("Unable to read file");
+
+fn main() {
+    let input: String = get_input("Please type path...");
+    let data = fs::read_to_string(input).expect("Unable to read file");
     println!("{}", data);
 }
 
-fn main() {
-    let path: String = get_input("Please type something...");
-    read_a_file(path);
+mod simple_user_input {
+    use std::io;
+    pub fn get_input(prompt: &str) -> String{
+        println!("{}",prompt);
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_goes_into_input_above) => {},
+            Err(_no_updates_is_fine) => {},
+        }
+        input.trim().to_string()
+    }
 }
