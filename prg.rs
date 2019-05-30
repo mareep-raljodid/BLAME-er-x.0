4c50a49dba48a2a0db000fe4b2d35ff53e13b40a
Rajdeep Bandopadhyay <rajdeepbandopadhyay@gmail.com>
2019-05-30 08:08:41 +0530

    cnages to read

--- prg.rs
+++ prg.rs
@@ -1,4 +1,13 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+fn read_a_file(path: String){
+let data = fs::read_to_string(path).expect("Unable to read file");
+    println!("{}", data);
+}
+
 fn main() {
-    let 
-    println!("Hello, world!");
+    let path: String = get_input("Please type something...");
+    read_a_file(path);
 }
