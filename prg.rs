1e7024a3fe598246ee64e78ae1dd995b0fdba6d3
Rajdeep Bandopadhyay <rajdeepbandopadhyay@gmail.com>
2019-05-30 16:22:33 +0530

    adding amorphism

--- prg.rs
+++ prg.rs
@@ -9,7 +9,7 @@ mod simple_user_input {
         match io::stdin().read_line(&mut input) {
             Ok(_goes_into_input_above) => {},
             Err(_no_updates_is_fine) => {},
-        }
+    }
     input.trim().to_string()
     }
 }
