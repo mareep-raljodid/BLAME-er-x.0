f8130372781ad0950c5b43151f9fc2f0752d66e0
Rajdeep Bandopadhyay <rajdeepbandopadhyay@gmail.com>
2019-06-07 00:43:15 +0530

    verification function: getting close to completion

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@ fn main() {
     println!("Please select below option: ");
     println!("  Enter 1 for PATH input of the required text file. ");
     println!("  Enter 2 for direct string input.");
+    println!("  Enter 3 for CHECKSUM verification of a file/string.");
+
 
     let op: String = get_input("Your choice: ");
     println!("Your choice was {}", op);
@@ -59,6 +61,33 @@ fn main() {
         println!("{:x}", dhash);
         fs::write("output.txt", dhash).expect("Unable to write file");
         println!("Output was successfully written to output.txt.");
+    }
+    if op == "3"
+    {
+        let userp: String = get_input("Please tell us if you are trying to verify a file object or a string (f/s).");
+        if userp == "f"
+        {
+            let fsdd: String = get_input("Please type the PATH along with filename and extention.");
+            let correcthash: String = get_input("Please paste in the required hash that your file needs to be checked with.");
+            let fcg = fs::read_to_string(fsdd).expect("Unable to read file, please try again.");
+            let goodcontent = String::from(fcg);
+            println!("Do you want to display file contents? (y/n) : Default: n");
+            let d: String = get_input("Your Choice: ");
+            if d != "n"{
+            println!("Content of the files are below: ");
+            println!("{}", goodcontent);
+            use blake2::{Blake2b, Digest};
+            let mut hasher = Blake2b::new();
+            let dataxt = goodcontent.as_bytes();
+            hasher.input(dataxt);
+            let checkhash = hasher.result();
+            if checkhash == correcthash
+            {
+                
+            }
+
+        }
+        }
     }
 }
 
