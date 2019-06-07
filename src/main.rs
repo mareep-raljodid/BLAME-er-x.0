51efeccfd566b81b6eb2ff329814202b8efaf9d0
Rajdeep Bandopadhyay <rajdeepbandopadhyay@gmail.com>
2019-06-07 18:30:54 +0530

    this is nice

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,17 @@ pub fn utf8_to_string(bytes: &[u8]) -> String {
 
 
 fn main() {
+
+    let scr ="
+______ _       ___  ___  ___ _____                        _____ 
+| ___ | |     / _ | |  |/  ||  ___|                      |  _  |
+| |_/ / |    / /_| || .  . || |__ ______ ___ _ __  __  __| |/' |
+| ___ | |    |  _  || ||/| ||  __|______/ _ | '__| | |/ /|  /| |
+| |_/ / |____| | | || |  | || |___     |  __/ |     >  < | |_/ /
+|____/|_____/|_| |_/|_|  |_/|____/      |___|_|    /_/|_(_)___/ 
+                                                                
+                                                                ";
+    println!("{} \n An HASH Digest and verification helper. \nDeveloped by Rajdeep Bandopadhyay \n © 2019, All rights Reserved.",scr);
     println!("You are currently in this PATH: {:?}", std::env::current_exe());
     println!("Please select below option: ");
     println!("  Enter 1 for PATH input of the required text file. ");
@@ -95,12 +106,16 @@ fn main() {
             if a.trim() == b.trim()
             {
                 println!("FILE generated HASH is {}, and provided HASH is {}", a, b);
+                println!();
+                println!();
                 println!("The required file PASSED verification with provided hash: ✅ VERIFIED.");
             }
-            
+
             else
             {
                 println!("FILE generated HASH is {}, and provided HASH is {}", a, b);
+                println!();
+                println!();
                 println!("The required file FAILED verification with provided hash: ❌ NOT VERIFIED.");            
             }
         }
